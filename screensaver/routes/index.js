91aaecff246a293fae973ac2a2ce069d3bce08aa
Minho Lee <lkjjr0424@hanmail.net>
2020-12-07 00:50:30 +0900

    confirm exception handling

--- screensaver/routes/index.js
+++ screensaver/routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
 function makeid(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
@@ -15,19 +16,28 @@ var rekognition = new AWS.Rekognition();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  var collectionID = makeid(8)
+  var collectionID = makeid(8);
   var params = {
-    CollectionId: collectionID
-   };
-   rekognition.createCollection(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-   });
-   console.log(collectionID)
-  res.render("index", { title: "Express", error:"",collection: collectionID});
+    CollectionId: collectionID,
+  };
+  rekognition.createCollection(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    // an error occurred
+    else console.log(data); // successful response
+  });
+  console.log(collectionID);
+  res.render("index", {
+    title: "AI Screensaver",
+    error: "",
+    collection: collectionID,
+  });
 });
-router.post("/", function (req, res, next){
-  console.log(req.body.collection)
-  res.render("index", { title: "Express", error:"",collection: req.body.collection});
+router.post("/", function (req, res, next) {
+  console.log(req.body.collection);
+  res.render("index", {
+    title: "AI Screensaver",
+    error: "",
+    collection: req.body.collection,
+  });
 });
 module.exports = router;
